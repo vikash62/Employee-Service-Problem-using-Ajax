e8df774ba3e8171d5e3cb02aa24d0195f1c15ac7
Vikash <vikashpathak041@gmail.com>
2022-07-26 19:41:36 +0530

    uc complete

--- Timer.js
+++ Timer.js
@@ -11,4 +11,4 @@ function showSessionExpire(){
 
 console.log("Activity-A: Trigerring Activity-B at "+showTime());
 setTimeout(showSessionExpire,5000);
-console.log("Activity-A: Triggered Activity-B at "+showTime()+" will execute after 5 seconds")
+console.log("Activity-A: Triggered Activity-B at "+showTime()+" will execute after 5 seconds") 
